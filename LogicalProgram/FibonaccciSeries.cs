701e0ebbed3bd9007fcf6840c1be060e9c7170ba
singhtushar689 <singhtushar689@gmail.com>
2023-02-07 12:46:25 +0530

    [Tushar] add: Displaying a Fibonacci Series

---
+++ LogicalProgram/FibonaccciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class FibonaccciSeries
+    {
+        public void Series()
+        {
+            Console.WriteLine("Enter the number of terms you want in fibonnaci Series ");
+            int term = Convert.ToInt32(Console.ReadLine());
+            int firstNum = 0; int secondNum = 1; int sum = 0, thirdNum = 0;
+            Console.Write(firstNum + " " + secondNum + " ");
+            
+            for (int i = 3; i <= term; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                Console.Write(thirdNum + " ");
+                firstNum = secondNum;
+                secondNum = thirdNum;
+            }
+        }
+    }
+}
---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Option Given Below: \n1.FibonacciSeries \n Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonaccciSeries fibonacci = new FibonaccciSeries();
+                    fibonacci.Series();
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
